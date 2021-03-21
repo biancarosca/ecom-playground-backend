ab230ea44ad456c3682eacdf427a5eaaa60fe11c
Bianca <rosca.bianca@yahoo.com>
2021-03-22 00:19:16 +0200

    set cors header

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,11 @@ const productsRouter = require("./routers/productsRouter");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 app.use(cors());
 app.use(express.json());
 app.use("/api",productsRouter);
