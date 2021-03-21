3482ffe69d1086cdc99a31f0fba3888df054fa85
Bianca <rosca.bianca@yahoo.com>
2021-03-22 00:24:07 +0200

    router cors header

--- backend/routers/productsRouter.js
+++ backend/routers/productsRouter.js
@@ -4,8 +4,13 @@ const getProducts = require("../controllers/getProducts");
 const addProduct = require("../controllers/addProduct");
 const handleNewProduct = require("../controllers/handleNewProduct");
 
-router.get("/products",getProducts);
-router.post("/products",addProduct);
+router.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
+});
+
+router.get("/products", getProducts);
+router.post("/products", addProduct);
 router.get("/new", handleNewProduct);
 
 module.exports = router;
