3040a5eb8923ce75ba3464508b3b702537ea6763
Bianca <rosca.bianca@yahoo.com>
2021-03-22 11:34:09 +0200

    cors error

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -4,10 +4,6 @@ const getProducts = require("../controllers/getProducts");
 const addProduct = require("../controllers/addProduct");
 const handleNewProduct = require("../controllers/handleNewProduct");
 
-router.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	next();
-});
 
 router.get("/products", getProducts);
 router.post("/products", addProduct);
--- server.js
+++ server.js
@@ -14,12 +14,7 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	next();
-});
-
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(express.json());
 app.use("/api", productsRouter);
 
