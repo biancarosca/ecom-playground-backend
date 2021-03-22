0657bfe557acbcff7aee6b52dd74e9a6aac1a40e
Bianca <rosca.bianca@yahoo.com>
2021-03-22 15:59:34 +0200

    remove slash

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use((req, res, next) => {
 	res.setHeader(
 		"Access-Control-Allow-Origin",
-		"https://ecom-playground-frontend.netlify.app/"
+		"https://ecom-playground-frontend.netlify.app"
 	);
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS");
