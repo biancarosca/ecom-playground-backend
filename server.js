c3126784afc55417ef86fc5f58313fe1e47ef340
Bianca <rosca.bianca@yahoo.com>
2021-03-22 12:18:19 +0200

    cors

--- server.js
+++ server.js
@@ -14,16 +14,9 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-app.use(cors());
+app.use(cors({origin: "https://ecom-playground-frontend.netlify.app"}));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://ecom-playground-frontend.netlify.app");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 
 
 
