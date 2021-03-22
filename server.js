393823e161de077bc9fcbe3053a82cf1c4a9daff
Bianca <rosca.bianca@yahoo.com>
2021-03-22 13:28:35 +0200

    remove frontend

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 
 if (process.env.NODE_ENV === "production") {
 
-	app.use(express.static("client","public"));
+	app.use(express.static("client"));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client","public", "index.html"));
+		res.sendFile(path.resolve(__dirname, "client","index.html"));
 	});
 }
 
