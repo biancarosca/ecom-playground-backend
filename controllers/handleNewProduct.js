0a084ac43a4a95076339671a50d28a6f9025f776
Bianca <rosca.bianca@yahoo.com>
2021-03-22 17:34:07 +0200

    set header in new

--- controllers/handleNewProduct.js
+++ controllers/handleNewProduct.js
@@ -1,12 +1,18 @@
 const Product = require("../models/productModel");
 
 const handleNewProduct = (req, res) => {
-    res.setHeader("Content-Type", "text/event-stream");
+	res.setHeader("Content-Type", "text/event-stream");
+	res.setHeader(
+		"Access-Control-Allow-Origin",
+		"https://ecom-playground-frontend.netlify.app"
+	);
 	const productEventEmitter = Product.watch();
-    const sendProduct = (data) => {
-        res.write(`data: ${data}\n\n`);
-    }
-	productEventEmitter.on('change', change => sendProduct(JSON.stringify(change)))
+	const sendProduct = (data) => {
+		res.write(`data: ${data}\n\n`);
+	};
+	productEventEmitter.on("change", (change) =>
+		sendProduct(JSON.stringify(change))
+	);
 };
 
 module.exports = handleNewProduct;
