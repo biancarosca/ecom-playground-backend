c4daf99951df3e7dfd644f29fd68d7d029ba6789
Bianca <rosca.bianca@yahoo.com>
2021-03-22 13:14:42 +0200

    commit all

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 
 if (process.env.NODE_ENV === "production") {
 
-	app.use(express.static("client"));
+	app.use(express.static("client","build"));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "index.html"));
+		res.sendFile(path.resolve(__dirname, "client","build", "index.html"));
 	});
 }
 
-app.use(cors({origin: "https://ecom-playground-frontend.netlify.app"}));
+app.use(cors());
 app.use(express.json());
 
 
