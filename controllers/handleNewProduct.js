990cb0cc598269a2f74c605fb2090ef617926928
Bianca <rosca.bianca@yahoo.com>
2021-03-22 22:33:48 +0200

    fixed errors

--- controllers/handleNewProduct.js
+++ controllers/handleNewProduct.js
@@ -2,18 +2,16 @@ const Product = require("../models/productModel");
 
 const handleNewProduct = (req, res) => {
 	res.setHeader("Content-Type", "text/event-stream");
+	res.setHeader("Cache-Control", "no-transform");
+	res.connection.setTimeout(0);
 	const productEventEmitter = Product.watch();
 	const sendProduct = (data) => {
-        res.setHeader(
-            "Access-Control-Allow-Origin",
-            "*"
-        );
 		res.write(`data: ${data}\n\n`);
 	};
 	productEventEmitter.on("change", (change) =>
 		sendProduct(JSON.stringify(change))
 	);
-    
+    res.write("data:");
 };
 
 module.exports = handleNewProduct;
