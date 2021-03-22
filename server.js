dac2283bff0bdecc660756f8bc9e95457fcb70de
Bianca <rosca.bianca@yahoo.com>
2021-03-22 12:03:04 +0200

    cors error

--- server.js
+++ server.js
@@ -14,13 +14,19 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
+app.use(cors());
+app.use(express.json());
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://ecom-playground-frontend.netlify.app/");
+  res.header("Access-Control-Allow-Origin", "https://ecom-playground-frontend.netlify.app");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
-app.use(cors({origin: null}));
-app.use(express.json());
+
+
 app.use("/api", productsRouter);
 
 const PORT = process.env.PORT || 5000;
