3c1ca4392f6775b0c0321e4a11483108fd03e561
Bianca <rosca.bianca@yahoo.com>
2021-03-22 11:27:33 +0200

    heroku error fix

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+if(process.env.NODE_ENV !== "production")
+	require("dotenv").config();
 
 const connectDB = async () => {
 	try {
--- server.js
+++ server.js
@@ -5,15 +5,24 @@ const productsRouter = require("./routers/productsRouter");
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+
+	app.use(express.static("client"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "index.html"));
+	});
+}
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
+});
 
 app.use(cors());
 app.use(express.json());
-app.use("/api",productsRouter);
+app.use("/api", productsRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
