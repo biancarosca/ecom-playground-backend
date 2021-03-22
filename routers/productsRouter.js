6f3908bfa1e9f4e77f83e3fbb54aa1e4d8a5a59f
Bianca <rosca.bianca@yahoo.com>
2021-03-22 15:37:07 +0200

    router header

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -5,6 +5,16 @@ const addProduct = require("../controllers/addProduct");
 const handleNewProduct = require("../controllers/handleNewProduct");
 
 
+router.use((req, res, next) => {
+	res.setHeader(
+		"Access-Control-Allow-Origin",
+		"https://ecom-playground-frontend.netlify.app/"
+	);
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS");
+	next();
+});
+
 router.get("/products", getProducts);
 router.post("/products", addProduct);
 router.get("/new", handleNewProduct);
