8eaf9ec4fa7cbf2e95ca2f441bccb3e0f2c717ba
Bianca <rosca.bianca@yahoo.com>
2021-03-22 15:16:28 +0200

    set header

--- server.js
+++ server.js
@@ -6,19 +6,23 @@ const productsRouter = require("./routers/productsRouter");
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-
 	app.use(express.static("client"));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client","index.html"));
+		res.sendFile(path.resolve(__dirname, "client", "index.html"));
 	});
 }
 
 app.use(cors());
 app.use(express.json());
 
-
-
+app.use((req, res, next) => {
+	res.setHeader(
+		"Access-Control-Allow-Origin",
+		"https://ecom-playground-frontend.netlify.app/"
+	);
+	next();
+});
 
 app.use("/api", productsRouter);
 
