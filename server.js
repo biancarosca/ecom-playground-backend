cc2e6a48ca4954a5c928579f7af1237dc03f5b4c
Bianca <rosca.bianca@yahoo.com>
2021-03-26 12:32:47 +0200

    socket.io added

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 require("./db/connect");
 const productsRouter = require("./routers/productsRouter");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+});
+const Product = require("./models/productModel");
 
-const app = express();
-
-app.use(cors({origin: "*"}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api", productsRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+let connectedSocket;
+
+io.on("connection", (socket) => {
+	connectedSocket = socket;
+});
+
+const productEventEmitter = Product.watch();
+const sendProduct = (data) => connectedSocket.emit("message", data);
+
+productEventEmitter.on("change", (change) => sendProduct(change));
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
